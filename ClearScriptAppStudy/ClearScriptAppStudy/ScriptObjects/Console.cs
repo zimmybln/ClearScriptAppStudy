d2c33a9119bb8b73ff9cfbf75e07c4006a29804f
Torsten Zimmermann <zimmy@web.de>
2021-01-06 11:26:10 +0100

    Verbesserung der Console

--- ClearScriptAppStudy/ClearScriptAppStudy/ScriptObjects/Console.cs
+++ ClearScriptAppStudy/ClearScriptAppStudy/ScriptObjects/Console.cs
@@ -30,5 +30,54 @@ namespace ClearScriptAppStudy.ScriptObjects
         {
             writeLineAction(format, OutputTypes.Error);
         }
+
+        public void log(params object[] args)
+        {
+            StringBuilder builder = new StringBuilder();
+
+            foreach(object arg in args)
+            {
+                if (arg == null)
+                    continue;
+
+                if (arg.GetType().IsValueType || (arg is String))
+                {
+                    builder.Append(arg.ToString());
+                }
+                else
+                {
+                    builder.Append(GetDescription(arg));
+                }
+            }
+
+            writeLineAction(builder.ToString(), OutputTypes.Info);
+        }
+
+        private string GetDescription(object item)
+        {
+            StringBuilder builder = new StringBuilder();
+
+            foreach(var property in item.GetType().GetProperties())
+            {
+                if (!property.CanRead)
+                    continue;
+
+                if (builder.Length > 0)
+                    builder.Append(", ");
+
+                try
+                {
+                    builder.AppendFormat("{0}: {1}", property.Name, property.GetValue(item));
+                }
+                catch (Exception ex)
+                {
+                                        
+                }
+            }
+
+
+
+            return builder.ToString();
+        }
     }
 }
